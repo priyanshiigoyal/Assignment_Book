6ffad602334685fc00815831e53e6ba002d66c65
piyua <priyanshi.goyal_cs18@gla.ac.in>
2019-09-21 21:09:26 +0530

    create definition class and declared variables

---
+++ src/book/Book.java
@@ -0,0 +1,11 @@
+package book;
+
+public class Book {
+    private String b_name, author, isbn;
+
+    Book(String nam, String auth, String ISBN) {
+        this.b_name = nam;
+        this.author = auth;
+        this.isbn = ISBN;
+    }
+}
