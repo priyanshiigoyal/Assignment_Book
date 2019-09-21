c57a82cf7004aa936c9df65143fef16fd6065015
piyua <priyanshi.goyal_cs18@gla.ac.in>
2019-09-21 21:10:05 +0530

    created getter and setter methods

--- src/book/Book.java
+++ src/book/Book.java
@@ -8,4 +8,36 @@ public class Book {
         this.author = auth;
         this.isbn = ISBN;
     }
+
+    public String getB_name() {
+        return b_name;
+    }
+
+    public void setB_name(String b_name) {
+        this.b_name = b_name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String toString() {
+        return "-----------------------------\n" +
+                "Book Name:\t" + b_name +
+                "\nAuthor Name:\t" + author +
+                "\nISBN:\t" + isbn +
+                "\n-----------------------------";
+    }
 }
